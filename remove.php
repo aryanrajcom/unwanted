8f70b3cb1f95e4113ea3802f2b7b396e26b3cf19
Aryan Raj <contact@aryanraj.com>
2019-07-28 16:36:26 +0530

    Create remove.php

---
+++ remove.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Unwanted
+ * Plugin URI:  https://github.com/rockingaryan/unwanted
+ * Description: Remove Unwanted Features from WordPress.
+ * Author:      Aryan Raj
+ * Author URI:  https://www.aryanraj.com/
+ * Version:     1.0.0
+ * License:     GPL-2.0+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package Unwanted
+ */
+ 
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
